f59af477787a59bc41bee5dcf902092e02c8f014
MariaMadrelejosMJPPM <madrelejos.mp@obmontessori.edu.ph>
2026-01-20 13:26:27 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function add() {
+    var num1 = document.getElementById('num1').value;
+    var num2 = document.getElementById('num2').value;
+    var sum = Number(num1) + Number(num2);
+        window.alert(sum);
+}
+function subtract() {
+    var num1 = document.getElementById('num1').value;
+    var num2 = document.getElementById('num2').value;
+    var diff = Number(num1) - Number(num2);
+        window.alert(diff);
+}
+function multiply() {
+    var num1 = document.getElementById('num1').value;
+    var num2 = document.getElementById('num2').value;
+    var product = Number(num1) * Number(num2);
+        window.alert(product);
+}
+function division() {
+    var num1 = document.getElementById('num1').value;
+    var num2 = document.getElementById('num2').value;
+    var quotient = Number(num1) / Number(num2);
+        window.alert(quotient);
+}
